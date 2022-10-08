74136efef18a785faf69da810e73418adbc79208
Owyn <Owyn@users.noreply.github.com>
2022-10-08 14:30:48 +0300

    fix #494

--- HandyImage.user.js
+++ HandyImage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name		Handy Image
-// @version		2022.10.04
+// @version		2022.10.08
 // @author		Owyn
 // @contributor	ubless607, bitst0rm
 // @namespace	handyimage
@@ -1263,7 +1263,7 @@ function makeworld()
 		break;
 	case "imgur.com":
 		j = true;
-		f = document.querySelectorAll(".image-placeholder,.post-image-placeholder,.image.post-image > img"); // for beta and non-beta
+		f = document.querySelectorAll(".image-placeholder:not([src*='maxwidth=']),.post-image-placeholder,.image.post-image > img"); // for beta and non-beta
 		if(f.length)
 		{
 			if(f.length != 1)
