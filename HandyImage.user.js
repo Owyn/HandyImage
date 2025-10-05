ccc9dbf402c9f8693d20b174e541049d77edac38
Owyn <Owyn@users.noreply.github.com>
2025-10-05 16:03:06 +0300

    fix #622 for Chrome

--- HandyImage.user.js
+++ HandyImage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name		Handy Image
-// @version		2025.10.05
+// @version		2025.10.06
 // @author		Owyn
 // @contributor	ubless607, bitst0rm
 // @namespace	handyimage
@@ -1338,6 +1338,7 @@ function makeworld()
 			let date = new Date();
 			date.setTime(date.getTime() + (6 * 60 * 60 * 1000));
 			expires = "; expires=" + date.toUTCString();
+			document.cookie = "nsfw_inter=1" + expires + "; path=/"; // for chrome
 			document.cookie = "sfw_inter=1" + expires + "; path=/";
 			i.click();
 			break
